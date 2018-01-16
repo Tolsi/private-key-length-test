049b42d48ffcbe269998c83997a5d34f326998f8
Sergey Tolmachev <tolsi.ru@gmail.com>
2018-01-16 19:54:48 +0300

    added generating network addresses functions

--- src/main/scala/ru/tolsi/keys/EthereumWrongSizeKey.scala
+++ src/main/scala/ru/tolsi/keys/EthereumWrongSizeKey.scala
@@ -1,11 +1,23 @@
 package ru.tolsi.keys
 
 import org.ethereum.crypto.ECKey
+import org.spongycastle.util.encoders.Hex
+
 
 object EthereumWrongSizeKey extends App {
-  val pk = 1.toByte +: Array.fill(39)(0.toByte)
+  def addressFromPublicKey(pk: Array[Byte]): String = {
+    Hex.toHexString(ECKey.fromPublicOnly(pk).getAddress)
+  }
+
+  val pk = Hex.decode("e65f46f09a967c2c01d19be33163b2f62e34d8d23d0637b9e063cd3f2f194dcd63f28dc1112c1cf8")
   val data = Array.fill(32)(1.toByte)
   val ecKey = ECKey.fromPrivate(pk)
-  val signature = ecKey.doSign(data)
+
+  println(s"Private key ${pk.length} bytes: ${Hex.toHexString(pk)}")
+  println(s"Fake private key (it's not used?) ${ecKey.getPrivKeyBytes.length} bytes: ${Hex.toHexString(ecKey.getPrivKeyBytes)}")
+  println(s"Public key ${ecKey.getPubKey.length} bytes: ${Hex.toHexString(ecKey.getPubKey)}")
+  println(s"Address: 0x${addressFromPublicKey(ecKey.getPubKey)}")
+
+  val signature = ecKey.sign(data)
   require(ecKey.verify(data, signature))
 }
--- src/main/scala/ru/tolsi/keys/WavesWrongSizeKey.scala
+++ src/main/scala/ru/tolsi/keys/WavesWrongSizeKey.scala
@@ -2,10 +2,14 @@ package ru.tolsi.keys
 
 import java.lang.reflect.Constructor
 
+import org.spongycastle.util.encoders.Hex
 import org.whispersystems.curve25519.OpportunisticCurve25519Provider
+import scorex.crypto.encode.Base58
+import scorex.crypto.hash.{Blake2b256, Keccak256}
 import scorex.crypto.signatures.Curve25519
 
 object WavesWrongSizeKey extends App {
+
   val provider: OpportunisticCurve25519Provider = {
     val constructor = classOf[OpportunisticCurve25519Provider]
       .getDeclaredConstructors
@@ -14,8 +18,31 @@ object WavesWrongSizeKey extends App {
     constructor.setAccessible(true)
     constructor.newInstance()
   }
-  val pk = 1.toByte +: Array.fill(39)(0.toByte)
+
+  val AddressVersion: Byte = 1
+  val ChecksumLength = 4
+  val HashLength = 20
+
+  def checkSum(withoutChecksum: Array[Byte]): Array[Byte] = hash(withoutChecksum).take(ChecksumLength)
+
+  def hash(bytes: Array[Byte]): Array[Byte] = {
+    scorex.utils.HashHelpers.applyHashes(bytes, Blake2b256, Keccak256)
+  }
+
+  def addressFromPublicKey(network: Byte, publicKey: Array[Byte]): String = {
+    val publicKeyHash = hash(publicKey).take(HashLength)
+    val withoutChecksum = AddressVersion +: network +: publicKeyHash
+    val bytes = withoutChecksum ++ checkSum(withoutChecksum)
+    Base58.encode(bytes)
+  }
+
+  val pk = Hex.decode("e65f46f09a967c2c01d19be33163b2f62e34d8d23d0637b9e063cd3f2f194dcd63f28dc1112c1cf8")
   val pubkey = provider.generatePublicKey(pk)
+
+  println(s"Private key ${pk.length} bytes: ${Hex.toHexString(pk)}")
+  println(s"Public key ${pubkey.length} bytes: ${Hex.toHexString(pubkey)}")
+  println(s"Address: ${addressFromPublicKey('W', pubkey)}")
+
   val data = Array.fill(32)(1.toByte)
   val signature = Curve25519.sign(pk, data)
   require(provider.verifySignature(pubkey, data, signature))
